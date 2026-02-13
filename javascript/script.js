cb7d8d1dd568656163dcf9907fe4e44eaddb8c5b
Felipe <feliperodrigofariamendes@gmail.com>
2026-02-13 11:38:50 -0300

    alpha 1 finalizado, adiconado praticas de seo, alterado responsividade.

--- javascript/script.js
+++ javascript/script.js
@@ -2,6 +2,7 @@ const menuBtn = document.querySelector(".hamburgueMenu");
 const menuLinks = document.querySelector(".menuLinks");
 
 menuBtn.addEventListener("click", () => {
-  menuLinks.style.display = 
-    menuLinks.style.display === "block" ? "none" : "block";
+  if (window.innerWidth <= 800) {
+    menuLinks.classList.toggle("active");
+  }
 });
