4d02459e0196ce44c4f3fde4f8614018a3978d89
Felipe <feliperodrigofariamendes@gmail.com>
2026-02-25 08:35:35 -0300

    erro do menu hamburguer corrigido

--- javascript/script.js
+++ javascript/script.js
@@ -2,7 +2,5 @@ const menuBtn = document.querySelector(".hamburgueMenu");
 const menuLinks = document.querySelector(".menuLinks");
 
 menuBtn.addEventListener("click", () => {
-  if (window.innerWidth <= 800) {
-    menuLinks.classList.toggle("active");
-  }
+  menuLinks.classList.toggle("active");
 });
